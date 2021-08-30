816f2917489dea2328484dacd65a274148051a77
SajjadAemmi <sajjadaemmi@gmail.com>
2021-08-30 15:47:03 +0430

    auto download weights bugs fixed

--- download_weights.py
+++ download_weights.py
@@ -6,8 +6,8 @@ path = './weights'
 if not os.path.exists(path):
     os.makedirs(path)
 
-if not os.path.exists(os.path.join(path, 'detecion')):
-    os.makedirs(os.path.join(path, 'detecion'))
+if not os.path.exists(os.path.join(path, 'detection')):
+    os.makedirs(os.path.join(path, 'detection'))
 
 if not os.path.exists(os.path.join(path, 'recognition')):
     os.makedirs(os.path.join(path, 'recognition'))
@@ -17,10 +17,10 @@ resnet50_final_url = "https://drive.google.com/uc?id=10xlOuLk4BPw92TEJYaqmA6gidl
 shape_predictor_68_face_landmarks_url = "https://drive.google.com/uc?id=1z0nq0Ubf-AMrn3XMJE37IHkQaM9jk9nt"
 
 if __name__ == '__main__':
-    output = os.path.join(os.path.join(path, 'detecion'), 'mobilenet0.25_Final.pth')
+    output = os.path.join(os.path.join(path, 'detection'), 'mobilenet0.25_Final.pth')
     gdown.download(mobilenet0_25_Final_url, output, quiet=False)
 
-    output = os.path.join(os.path.join(path, 'detecion'), 'Resnet50_final.pth')
+    output = os.path.join(os.path.join(path, 'detection'), 'Resnet50_final.pth')
     gdown.download(resnet50_final_url, output, quiet=False)
 
     output = os.path.join(path, 'shape_predictor_68_face_landmarks.dat')
